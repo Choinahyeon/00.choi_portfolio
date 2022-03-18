9a9fd03b1f66d1cae82b635c8ee11ed136fbdf76
Choinahyeon <skgusdlekt@gmail.com>
2022-03-18 15:59:08 +0900

    .

---
+++ js/index.js
@@ -0,0 +1,51 @@
+// 버튼
+$(".hover").mouseleave(
+    function () {
+        $(this).removeClass("hover");
+    }
+);
+
+
+// 카운트
+$(function() {
+    var count0 = count1 = count2 = 0;
+
+    timeCounter();
+
+    function timeCounter() {
+
+        id0 = setInterval(count0Fn, 50);
+
+        function count0Fn() {
+        count0++;
+        if (count0 > 29) {
+            clearInterval(id0);
+        } else {
+            $(".number").eq(0).text(count0);
+        }
+
+        }
+
+        id1 = setInterval(count1Fn, 20);
+
+        function count1Fn() {
+        count1++;
+        if (count1 > 148) {
+            clearInterval(id1);
+        } else {
+            $(".number").eq(1).text(count1);
+        }
+        }
+
+        id2 = setInterval(count2Fn, 90);
+
+        function count2Fn() {
+        count2++;
+        if (count2 > 5) {
+            clearInterval(id2);
+        } else {
+            $(".number").eq(2).text(count2);
+        }
+        }
+    }
+    });
